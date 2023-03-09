e25ec6863f3ad4bbf73c9e5d55645d240783c115
Pratik Kumar <khoonipratik@gmail.com>
2023-03-09 14:53:52 +0530

    done by me

--- index.js
+++ index.js
@@ -6,3 +6,14 @@ function myFunction() {
       x.className = "topnav";
     }
   }
+  <div class="topnav" id="myTopnav">
+        <a href="/">Home</a>
+        <a href="#introduction" class="active">My Intro</a>
+        <a href="#ProJects">Projects</a>
+        <a href="#Skills">Skill</a>
+        <a href="#contact">Contact me</a>
+        <a href="./Images/Pratik_Kumar_resume.pdf" download>Resume</a>
+        <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+            <i class="fa fa-bars"></i>
+          </a>
+    </div> 
